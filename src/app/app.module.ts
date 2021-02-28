92b2ac8322729e61ca44de45a33d66fcdf2d4150
Aditya Inapurapu <adityaii@gmail.com>
2021-02-28 10:24:11 -0800

    Adding pokemon service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonService } from './pokemon.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [PokemonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from "../pokemon.service";
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.css']
+})
+export class PokemonListComponent implements OnInit {
+  private pokemonData: any;
+
+  constructor(private svc: PokemonService) { }
+
+  ngOnInit() {
+    this.svc.getPokemons().subscribe(data => {
+      this.pokemonData = data;
+    });
+  }
+
+}
